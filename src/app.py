3cd364339c462077d7a9071d5fbdf69db273f629
Vicente L <visanmar@gmail.com>
2025-02-27 14:10:29 +0100

    change mongo_uri 3

--- src/app.py
+++ src/app.py
@@ -15,8 +15,8 @@ username = os.getenv('MONGO_USERNAME')
 password = os.getenv('MONGO_PASSWORD')
 cluster = os.getenv('MONGO_CLUSTER')
 
-#app.config['MONGO_URI'] = 'mongodb+srv://' + username + ':' + password + '@cluster0.kewqy.mongodb.net/listacliente?retryWrites=true&w=majority&appName=Cluster0'
-app.config['MONGO_URI'] = 'mongodb+srv://visanmar:Uy9AEiw9zHpIK0Uh@cluster0.kewqy.mongodb.net/listacliente?retryWrites=true&w=majority&appName=Cluster0'
+app.config['MONGO_URI'] = 'mongodb+srv://' + username + ':' + password + '@cluster0.kewqy.mongodb.net/listacliente?retryWrites=true&w=majority&appName=Cluster0'
+#app.config['MONGO_URI'] = 'mongodb+srv://visanmar:Uy9AEiw9zHpIK0Uh@cluster0.kewqy.mongodb.net/listacliente?retryWrites=true&w=majority&appName=Cluster0'
 mongo = PyMongo(app)
 
 
